99622f0ac44986a1822f9bb6260d55b0fa6d978c
Robert Laws <roblaws@me.com>
2019-07-06 13:58:38 +0300

    begin basic react setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Welcome from './components/Welcome';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  state = {
+    friend: ''
+  }
+
+  componentDidMount() {
+    this.setState({
+      friend: 'Kal Kool'
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Welcome name={'Hal Hope'} />
+        <h4>friend: {this.state.friend}</h4>
+      </div>
+    )
+  }
 }
-
-export default App;
